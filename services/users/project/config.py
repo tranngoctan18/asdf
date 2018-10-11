72d293f5a8785731bf6ccd3fb9552eb83585ac2f
Tan <tranngoctan18@gmail.com>
2018-10-11 14:52:26 +0700

    first commit

---
+++ services/users/project/config.py
@@ -0,0 +1,23 @@
+import os
+
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SECRET_KEY = 'my_precious'
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
